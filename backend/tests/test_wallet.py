717c1a2f7ddeca5bcdd2b12debd7e2b5abca6ddb
Abhinav Mishra <abhinavmishra0302@gmail.com>
2023-05-18 11:48:57 +0530

    Valid chain

--- backend/tests/test_wallet.py
+++ backend/tests/test_wallet.py
@@ -1,15 +1,17 @@
 from inspect import signature
 from backend.wallet.wallet import Wallet
 
+
 def test_verify_valid_signature():
-    data = {'foo' : 'test_data'}
+    data = {'foo': 'test_data'}
     wallet = Wallet()
     signature = wallet.sign(data)
 
     assert Wallet.verify(wallet.public_key, data, signature)
 
+
 def test_verify_invalid_signature():
-    data = {'foo' : 'test_data'}
+    data = {'foo': 'test_data'}
     wallet = Wallet()
     signature = wallet.sign(data)
 
